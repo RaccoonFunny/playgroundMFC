30d06f06ec82c736068da578ba0ab055a4e1fa63
RaccoonF <koltashov.andrey@gmail.com>
2024-09-17 06:58:38 +0300

    Add project files.

---
+++ playgroundMFC/playgroundMFC.h
@@ -0,0 +1,32 @@
+﻿
+// playgroundMFC.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CplaygroundMFCApp:
+// Сведения о реализации этого класса: playgroundMFC.cpp
+//
+
+class CplaygroundMFCApp : public CWinApp
+{
+public:
+	CplaygroundMFCApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CplaygroundMFCApp theApp;
---
+++ playgroundMFC/playgroundMFCDlg.h
@@ -0,0 +1,48 @@
+﻿
+// playgroundMFCDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CplaygroundMFCDlg
+class CplaygroundMFCDlg : public CDialogEx
+{
+// Создание
+public:
+	CplaygroundMFCDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PLAYGROUNDMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedRadio1();
+	afx_msg void OnBnClickedRadio3();
+	afx_msg void OnBnClickedRadio2();
+	afx_msg void OnEnChangeEdit1();
+	void updateProgress(int percent);
+	afx_msg void OnNMCustomdrawProgress1(NMHDR* pNMHDR, LRESULT* pResult);
+	CEdit* m_pEditControl;
+	CProgressCtrl* pProgress;
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton1();
+	INT m_full;
+	INT m_half;
+	INT m_empty;
+};
